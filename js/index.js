7f49174d2d85e563830ec6fd44239e175572e848
Egor Nefedev <enefe815@yandex.ru>
2020-11-30 19:35:13 +0300

    добавляет к попапу js

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const editPopup = document.querySelector('.profile__edit');
+const closePopup = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
+
+const formElement = document.querySelector('.popup__container');
+const nameProfile = document.querySelector('.profile__name');
+const captionProfile = document.querySelector('.profile__caption');
+const nameInput = document.querySelector('.popup__name'); 
+const captionInput = document.querySelector('.popup__caption');
+
+editPopup.addEventListener('click', openPopup);
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+
+    nameInput.value = nameProfile.textContent;
+    captionInput.value = captionProfile.textContent;
+}
+
+closePopup.addEventListener('click', removePopup);
+
+function removePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler(event) {
+    event.preventDefault(); 
+
+    nameProfile.textContent = nameInput.value;
+    captionProfile.textContent = captionInput.value;
+
+    removePopup();
+}
